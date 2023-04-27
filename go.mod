842a194352062228076c85dc537f5676aa44a9a3
danielbene <danielbene@protonmail.com>
2023-04-27 19:26:59 +0200

    testing gocron

--- go.mod
+++ go.mod
@@ -1,8 +1,11 @@
 module restic-bot
 
-go 1.20
+go 1.19
 
 require (
-	github.com/mitchellh/mapstructure v1.5.0 // indirect
-	gopkg.in/yaml.v3 v3.0.1 // indirect
+	github.com/go-co-op/gocron v1.23.0
+	github.com/mitchellh/mapstructure v1.5.0
+	gopkg.in/yaml.v3 v3.0.1
 )
+
+require github.com/robfig/cron/v3 v3.0.1 // indirect
--- src/main.go
+++ src/main.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"os"
 	"restic-bot/util"
+	"time"
+
+	"github.com/go-co-op/gocron"
 )
 
 func main() {
@@ -14,4 +17,16 @@ func main() {
 
 	fmt.Printf("%+v\n", conf)
 	fmt.Println(conf.Look)
+
+	cron := gocron.NewScheduler(time.Local)
+
+	cron.Every(5).Seconds().Do(func() {
+		fmt.Println("yo")
+	})
+
+	cron.Every("8s").Do(func() {
+		fmt.Println("hai")
+	})
+
+	cron.StartBlocking()
 }
